8a6acccf5c57e26b1ef84e6985ba3f9bbeb189b8
Saquib Nasar Siddiqui <saquibsiddiqui023@gmail.com>
2024-07-07 10:36:57 +0530

    union

--- myUnion.ts
+++ myUnion.ts
@@ -10,3 +10,15 @@ type Admin = {
 
 // let saquib: User | Admin = { name: 2, id: 3334 };
 let saquib: User | Admin = { userName: "saquib", id: "f" };
+
+// you have anytype
+const data: (string | number)[] = [21, "3"];
+
+// all element has to be type you pass
+const data1: string[] | number[] = [21, 2];
+
+// this wrong way of doing it
+const data3: string[] | number[] = [21, "2"];
+
+// you can put direct value to varibale that can't be change
+let pi: 3.14 = 3.14;
