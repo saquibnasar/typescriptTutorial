f945fc4842ce00dbefc78197c1d4a8c7186279d3
Saquib Nasar Siddiqui <saquibsiddiqui023@gmail.com>
2024-07-10 18:01:45 +0530

    gettters and setters

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,32 @@
 class indexUser {
   readonly city: string = "delhi";
   world: string = "earth";
-
+  email: string = "g@.com";
+  _courseCout: number = 0;
   //   this can't be change out side of class or accessible
   private tryToChange: string =
     "can you change it outside of class and constructor";
 
   constructor(
-    public email: string,
+    email: string,
     public name: string // private way: string
   ) {
     this.email = email;
     this.name = name;
+  }
+
+  // getter use to get the value normally use to get private value
+  get getAppleEmail(): string {
+    return `apple ${this.email}`;
+  }
+
+  // setter use to set value of anything  and normally use to set private value
+
+  set setAppleEmail(courseNum: number) {
+    if (courseNum <= 1) {
+      throw new Error("course number can't be less then 1");
+    }
+    this._courseCout = courseNum;
   }
 }
 // bad way to write code
