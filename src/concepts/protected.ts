d8ae295da660469ebc12b8f8f5f22ef29539198c
Saquib Nasar Siddiqui <saquibsiddiqui023@gmail.com>
2024-07-11 20:00:03 +0530

    proteced

---
+++ src/concepts/protected.ts
@@ -0,0 +1,17 @@
+class protectedClass {
+  private name = "saquib";
+  protected protectedName = "saquib";
+
+  constructor() {}
+}
+
+class user extends protectedClass {
+  changeuserName() {
+    // private varibele can't be use out of it class
+    this.name = "heelo";
+    // proteced varibele can be use in extend class but not anywhere else
+    this.protectedName = "hello";
+  }
+}
+
+export {};
